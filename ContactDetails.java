fb21f27cfc1f6a046bac0b1654569c44dcef1959
gitanjali <gitaanjalide@gmail.com>
2022-07-27 14:45:46 +0530

    New contact

--- ContactDetails.java
+++ ContactDetails.java
@@ -1,68 +1,92 @@
 package com.bridgelabz.address.book;
+import java.util.Scanner;
 
 public class ContactDetails {
-	 private String firstName;
-     private String lastName;
-     private String address;
-     private String city;
-     private String state;
-     private String zip;
-     private String phoneNumber;
-     
+	private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    
 
-     public String getFirstName() {
-         return firstName;
-     }
+    public String getFirstName() {
+        return firstName;
+    }
 
-     public void setFirstName(String firstName) {
-         this.firstName = firstName;
-     }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-     public String getLastName(){
-         return lastName;
-     }
+    public String getLastName(){
+        return lastName;
+    }
 
-     public void setLastName(String lastName) {
-         this.lastName = lastName;
-     }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-     public String getAddress(){
-         return address;
-     }
+    public String getAddress(){
+        return address;
+    }
 
-     public void setAddress(String address) {
-         this.address = address;
-     }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-     public String getCity(){
-         return city;
-     }
+    public String getCity(){
+        return city;
+    }
 
-     public void setCity(String city) {
-         this.city = city;
-     }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-     public String getState(){
-         return state;
-     }
+    public String getState(){
+        return state;
+    }
 
-     public void setState(String state) {
-         this.state = state;
-     }
+    public void setState(String state) {
+        this.state = state;
+    }
 
-     public String getzip(){
-         return zip;
-     }
+    public String getzip(){
+        return zip;
+    }
 
-     public void setZip(String zip) {
-         this.zip = zip;
-     }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 
-     public String getPhoneNumber() {
-         return phoneNumber;
-     }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-     public void setPhoneNumber(String phoneNumber) {
-         this.phoneNumber = phoneNumber;
-     }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    Scanner sc = new Scanner(System.in);
+    public void createContacts() {
+        System.out.print(" Enter First Name : ");
+        setFirstName(sc.nextLine());
+        System.out.print(" Enter Last Name : ");
+        setLastName(sc.nextLine());
+        System.out.print(" Enter Address : ");
+        setAddress(sc.nextLine());
+        System.out.print(" Enter city : ");
+        setCity(sc.nextLine());
+        System.out.print(" Enter State : ");
+        setState(sc.nextLine());
+        System.out.print(" Enter Zip Code : ");
+        setZip(sc.nextLine());
+        System.out.print(" Enter Phone Number : ");
+        setPhoneNumber(sc.nextLine());
+        
+    }
+    
+    
+
+ 
 }
